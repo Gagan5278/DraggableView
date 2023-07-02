2da3955866f13d35888dc5bbe667589db49b9ef8
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-07-02 21:14:27 +0200

    Initial Commit

---
+++ DraggableView/DraggableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggableViewApp.swift
+//  DraggableView
+//
+//  Created by Gagan Vishal  on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct DraggableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
